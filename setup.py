fb941b5dbbfc2d4fefeff8d4a499fd0675a776cb
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2009-09-17 20:19:14 +0000

    create at setup time

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import sys
 from distutils.core import setup
+
+pyvers='%s.%s' % sys.version_info[0:2]
+d1='lib/python%s/site-packages' % pyvers
+d2='lib64/python%s/site-packages' % pyvers
+
+tablefile=open('ups/esutil.table','w')
+tablefile.write('%s\n' % d1)
+tablefile.write('%s\n' % d2)
+
+tablefile.write('envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)\n' % d1)
+tablefile.write('envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)\n' % d2)
+tablefile.close()
 
 # data_files copies the ups/esutil.table into prefix/ups
 setup(name='esutil',
