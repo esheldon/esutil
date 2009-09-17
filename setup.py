cf0e07ab2a5b1d014fb8a917a2772220528d988a
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2009-09-17 20:30:32 +0000

    oracle tools

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-import sys
+import sys,os
 from distutils.core import setup
 
 pyvers='%s.%s' % sys.version_info[0:2]
 d1='lib/python%s/site-packages' % pyvers
 d2='lib64/python%s/site-packages' % pyvers
 
+if not os.path.exists('ups'):
+    os.mkdir('ups')
 tablefile=open('ups/esutil.table','w')
-tablefile.write('%s\n' % d1)
-tablefile.write('%s\n' % d2)
-
+tablefile.write('setupOptional("python")\n');
 tablefile.write('envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)\n' % d1)
 tablefile.write('envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)\n' % d2)
 tablefile.close()
