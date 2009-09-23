887e85d2702e6a7b152ab47f49704d978c247f7a
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2009-09-23 02:23:32 +0000

    version info now available

---
+++ tags/v0.1.3/esutil/__init__.py
@@ -0,0 +1,44 @@
+# we don't import oracle_util by default as it probably will fail for 
+# most people
+
+
+# version info
+# You need to run 'svn propset svn:keywords HeadURL' on the file and commit
+# before this works.
+#
+# Don't edit these svn properties by hand
+
+_property_headurl='$HeadURL$'
+
+def get_esutil_version():
+    import os
+    from sys import stderr
+
+    thisname='/esutil/__init__.py'
+    badvers="NOTAG: unparseable"
+
+    psplit=_property_headurl.split()
+    if len(psplit) != 3:
+        mess="headurl did not split into 3: '%s'\n" % _property_headurl
+        stderr.write(mess)
+        return badvers
+
+    url=psplit[1]
+
+    if url.find(thisname) == -1:
+        mess="url '%s' does not contain string '%s'\n" % \
+                (_property_headurl, thisname)
+        stderr.write(mess)
+        return badvers
+
+    urlfront = url.replace(thisname, '')
+
+    tag = os.path.basename(urlfront)
+    return tag
+
+
+version=get_esutil_version()
+from esutil import xmltools
+from esutil import ostools
+from esutil import misc
+
