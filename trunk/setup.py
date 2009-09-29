ed04ebed1f35eb40d6d3ad73b65c54ed5193f76d
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2009-09-29 15:07:57 +0000

    explanation of default ups/esutil.table

--- trunk/setup.py
+++ trunk/setup.py
@@ -8,14 +8,21 @@ d2='lib64/python%s/site-packages' % pyvers
 if not os.path.exists('ups'):
     os.mkdir('ups')
 tablefile=open('ups/esutil.table','w')
-tablefile.write('setupOptional("python")\n');
-tablefile.write('envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)\n' % d1)
-tablefile.write('envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)\n' % d2)
+tab="""
+# The default version of this file will be overwritten on setup to include
+# paths determined from the python version.  This is useful to have in place
+# though so that dependencies can be checked *before* installation.  Currently
+# there are no required dependencies, so this is somewhat moot.
+
+setupOptional("python")
+envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)
+envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)
+""" % (d1,d2)
+tablefile.write(tab)
 tablefile.close()
 
 # data_files copies the ups/esutil.table into prefix/ups
 setup(name='esutil',
-      version='0.1',
       description='Erin Sheldons Utilities',
       packages=['esutil'],
       data_files=[('ups',['ups/esutil.table'])])
