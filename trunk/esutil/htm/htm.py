befb9c6bcc67a4d1385c2eb7788add4cc8628cf8
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2010-03-03 22:50:45 +0000

    added to HTM.match() ability to write results to a file and read them.

--- trunk/esutil/htm/htm.py
+++ trunk/esutil/htm/htm.py
@@ -1,9 +1,42 @@
+"""
+
+This is a Class to deal with the Heirarchical Triangular Mesh, which is a
+method for breaking the unit sphere into a tree structure where each node in
+the tree is represented by a spherical triangle.  The "depth" of the tree
+determines the size of the smallest triangle, with higher depths meaning
+smaller triangles.  Currently depths up to 13 are supported, which 
+corresponds to an area of
+
+    http://www.sdss.jhu.edu/htm/
+
+At this point two tasks can be peformed with this code: 
+    
+    1) Find the id of the triangle a point or set of points belongs to.
+
+    2) Match two sets of points to one another, returning lists of matches and
+    the separation distance, or alternatively write the data to a file.
+
+
+Examples:
+
+    >>> import esutil
+    >>> h = esutil.htm.HTM(
+
+"""
 import htmc
 from esutil import stat
 import numpy
 from sys import stdout
 
 class HTM(htmc.HTMC):
+
+    def area(self):
+        pi=numpy.pi
+        area0=4.0*pi/8.0
+
+        areadiv = 4.0**self.depth
+        area = 60.0**2 * area0/areadiv*(180.0/pi)**2
+        return area
 
     def match(self, ra1, dec1, ra2, dec2, radius, distance=False,
               maxmatch=0, 
