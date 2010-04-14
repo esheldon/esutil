26bd6b4318eb022a3677d82eff88b99ea507e40b
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2010-04-14 15:28:01 +0000

    use stdint for type declarations

--- esutil/htm/htm_src/SpatialGeneral.h
+++ esutil/htm/htm_src/SpatialGeneral.h
@@ -49,6 +49,26 @@
 // ------------------------------------------------------------------------
 //
 
+
+// using stdint is safer, especially for OSX.
+#include <stdint.h>
+
+typedef uint8_t uint8;
+typedef int8_t int8;
+
+typedef uint16_t uint16;
+typedef int16_t int16;
+
+typedef uint32_t uint32;
+typedef int32_t int32;
+
+typedef uint64_t uint64;
+typedef int64_t int64;
+
+typedef float			float32;
+typedef double			float64;
+
+/*
 #if !defined(OO_MACHINE_H) && !defined(OO_CONFIG_H) && !defined(SXGENERAL_H)
 typedef char			int8;
 typedef unsigned char		uint8;
@@ -66,6 +86,7 @@ typedef long long		int64;
 typedef unsigned long long	uint64;
 #endif
 #endif
+*/
 
 #define IDSIZE                     64
 #define HTMNAMEMAX                 32
