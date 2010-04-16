acba0db71c4a5ca173a296801f91ec5e49698612
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2010-04-16 15:55:51 +0000

    in htm fixed bug cause by OS X 10.6 int64_t having a different typeid name than npy_int64

--- esutil/htm/unit_tests.py
+++ esutil/htm/unit_tests.py
@@ -56,6 +56,7 @@ def test():
 
     # try the matching
     stdout.write('Matching by ra/dec, expect 10 matches ordered by distance....')
+    stdout.flush()
     two = 2.0/3600.
     # offset second list by fraction of 2 arcsec in dec
     # not last ones don'e match at all
