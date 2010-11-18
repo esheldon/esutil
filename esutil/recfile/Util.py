faaab3bedf2b28aa180305813077dabb88d2be44
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2010-11-18 23:20:13 +0000

    make sqlite util functional with arrays.  Was not able to get type sizes working

--- esutil/recfile/Util.py
+++ esutil/recfile/Util.py
@@ -529,6 +529,9 @@ class Recfile(object):
             r = records.Records(self.fobj, mode='u', delim=self.delim, 
                                 bracket_arrays=self.bracket_arrays)
             dataview = data.view(numpy.ndarray) 
+            # make sure the data are in native format.  This greatly 
+            # simplifies the C code
+            to_native_inplace(dataview)
             r.Write(dataview, padnull=self.padnull, ignorenull=self.ignorenull)
         else:
             # Write data out as a binary chunk
@@ -1555,5 +1558,52 @@ def isstring(obj):
         return True
     else:
         return False
+
+def to_native_inplace(array):
+    """
+    Convert to native byte ordering in place
+    """
+
+    if numpy.little_endian:
+        machine_little=True
+    else:
+        machine_little=False
+
+    data_little=False
+    if array.dtype.names is None:
+        data_little = is_little_endian(array)
+    else:
+        # assume all are same byte order: we only need to find one with
+        # little endian
+        for fname in array.dtype.names:
+            if is_little_endian(array[fname]):
+                data_little=True
+                break
+
+    if ( (machine_little and not data_little) 
+            or (not machine_little and data_little) ):
+
+        outdata = array.byteswap(True)
+        outdata.dtype = outdata.dtype.newbyteorder()
+
+
+def is_little_endian(array):
+    """
+    Return True if array is little endian. Note strings are neither big
+    or little endian.  The input must be a simple numpy array, not
+    an array with fields.
+
+    REVISION HISTORY:
+        Created 2009, Erin Sheldon, NYU.
+    """
+
+    if numpy.little_endian:
+        machine_little=True
+    else:
+        machine_little=False
+
+    byteorder = array.dtype.base.byteorder
+    return (byteorder == '<') or (machine_little and byteorder == '=')
+
 
 
