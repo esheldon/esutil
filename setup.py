a689dcb17c994e9b32cc1eb15c0b8e6fcc7f321a
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2011-04-05 19:56:18 +0000

    make sure includes get copied

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ def configuration(parent_package='',top_path=None):
     config = Configuration(None, parent_package, top_path)
 
     config.add_subpackage('esutil')
+    config.add_data_dir('esutil/include')
     return config
 
 def setup_package():
