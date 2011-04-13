19d172a81065ace66a417f97cd20eb65d3a11209
erin.sheldon <erin.sheldon@1caee9da-a3bf-11de-8a4e-6d34bb13bdfc>
2011-04-13 00:09:49 +0000

    fixed ups.  added docs

--- trunk/esutil/cosmology/__init__.py
+++ trunk/esutil/cosmology/__init__.py
@@ -1,2 +1,28 @@
+"""
+A package for calculating  cosmological distances.  
+
+Classes
+-------
+Cosmo:
+    This is the class that does all the calculating.  See docs on cosmology.Cosmo
+    for more details.
+
+    Cosmo Class Methods
+    --------------
+    DH: Return the hubble distance.
+    Dc: Comoving distance.
+    Dm: Transverse comoving distance.
+    Da: Angular diameter distance.
+    Dl: Luminosity distance.
+    dV: Volume element.
+    V:  Volume between two redshifts.
+    distmod: Distance modulus.
+    sigmacritinv: Inverse critical density for lensing.
+
+    Ez_inverse: Calculate 1/E(z)
+    Ezinv_integral: Calculate the integral of 1/E(z) from zmin to zmax
+
+
+"""
 from . import cosmology
 from .cosmology import Cosmo
