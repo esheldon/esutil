74bc6daa457127ef2b73b82c24b980ad3e0feedd
Erin Sheldon <erin.sheldon@gmail.com>
2015-07-09 12:11:44 -0400

    added Grid for plotting

--- esutil/plotting.py
+++ esutil/plotting.py
@@ -1050,3 +1050,79 @@ def fake_filled_circles(labels, **keys):
 
     return fake_points(['filled circle']*len(labels), labels,
                        **keys)
+
+class Grid(object):
+    """
+    represent plots in a grid.  The grid is chosen
+    based on the number of plots
+    
+    example
+    -------
+    grid=Grid(n)
+
+    for i in xrange(n):
+        row,col = grid(i)
+
+        # equivalently grid.get_rowcol(i)
+
+        plot_table[row,col] = plot(...)
+    """
+    def __init__(self, nplot):
+        self.set_grid(nplot)
+    
+    def set_grid(self, nplot):
+        """
+        set the grid given the number of plots
+        """
+        from math import sqrt
+
+        self.nplot=nplot
+
+        # first check some special cases
+        if nplot==8:
+            self.nrow, self.ncol = 2,4
+        else:
+
+            sq=int(sqrt(nplot))
+            if nplot==sq*sq:
+                self.nrow, self.ncol = sq,sq
+            elif nplot <= sq*(sq+1):
+                self.nrow, self.ncol = sq,sq+1
+            else:
+                self.nrow, self.ncol = sq+1,sq+1
+
+        self.nplot_tot=self.nrow*self.ncol
+
+    def get_rowcol(self, index):
+        """
+        get the grid position given the number of plots
+
+        move along columns first
+
+        parameters
+        ----------
+        index: int
+            Index in the grid
+
+        example
+        -------
+        nplot=7
+        grid=Grid(nplot)
+        arr=biggles.FramedArray(grid.nrow, grid.ncol)
+
+        for i in xrange(nplot):
+            row,col=grid.get_rowcol(nplot, i)
+            arr[row,col].add( ... )
+        """
+
+        imax=self.nplot_tot-1
+        if index > imax:
+            raise ValueError("index too large %d > %d" % (index,imax))
+
+        row = index/self.ncol
+        col = index % self.ncol
+
+        return row,col
+
+    def __call__(self, index):
+        return self.get_rowcol(index)
