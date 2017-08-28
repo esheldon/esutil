d19b8f0169c53d6a9447c0495da378edf112c359
Erin Sheldon <erin.sheldon@gmail.com>
2017-08-28 17:04:54 -0400

    another minor bump for new pypi issues

--- esutil/__init__.py
+++ esutil/__init__.py
@@ -92,7 +92,7 @@ _property_headurl='$HeadURL$'
 
 import sys
 
-__version__ = "0.6.2.1"
+__version__ = "0.6.2.2"
 
 def version():
     return __version__
--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ classifiers = [
 
 
 setup(name='esutil',
-      version="0.6.2.1",
+      version="0.6.2.2",
       author="Erin Scott Sheldon",
       author_email="erin.sheldon@gmail.com",
       classifiers=classifiers,
