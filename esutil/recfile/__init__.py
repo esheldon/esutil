e6b4047f8986fc60460fbccf77da9bb05f5d7620
Erin Sheldon <erin.sheldon@gmail.com>
2021-05-13 22:24:23 -0400

    remove old test

--- esutil/recfile/__init__.py
+++ esutil/recfile/__init__.py
@@ -2,7 +2,6 @@ from .import Util
 from .Util import Recfile
 from .Util import write, read
 from .Util import Open
-from .test import test
 
 # use the same doc as the Util module
 __doc__=Util.__doc__
