10b4ecc3429b8070112d03da35ff53d214e56478
Erin Sheldon <erin.sheldon@gmail.com>
2021-05-13 21:00:32 -0400

    convert integrate tests to pytest

---
+++ esutil/tests/test_integrate.py
@@ -0,0 +1,10 @@
+import esutil as eu
+
+
+def test_integrate():
+    npts=10
+    x, w = eu.integrate.gauleg(1.0, 2.0, npts)
+
+    assert eu.integrate.have_cgauleg
+    assert x.size == npts
+    assert w.size == npts
