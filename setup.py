b81022752b953714fc881c52126a834c2a8157e8
Erin Sheldon <erin.sheldon@gmail.com>
2021-05-13 23:03:57 -0400

    fix doc

--- setup.py
+++ setup.py
@@ -228,25 +228,10 @@ if have_numpy:
     packages.append('esutil.integrate')
 
 
-    packages.append('esutil.unit_tests')
-
 long_description="""
 A python package including a wide variety of utilities, focused primarily on
 numerical python, statistics, and file input/output.   Includes specialized
 tools for astronomers.
-
-For full documentation see the project web site http://code.google.com/p/esutil/
-
-
-Tests
------
-
-All tests should pass
-
-```python
-import esutil as eu
-eu.test()
-```
 """
 
 classifiers = [
