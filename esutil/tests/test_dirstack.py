8a2bd9cd08c21f4e9bc8a94bf8682bf75af6c2ed
Erin Sheldon <erin.sheldon@gmail.com>
2026-06-28 13:52:12 -0400

    try to work around macos oddness

--- esutil/tests/test_dirstack.py
+++ esutil/tests/test_dirstack.py
@@ -1,34 +1,42 @@
 def test_dirstack_context():
     import tempfile
     import os
-    from os.path import abspath
     from esutil.ostools import DirStack
 
     with tempfile.TemporaryDirectory() as tmpdir:
 
-        # try abspath to get tests to work on macos
+        # on macos CI the actual directory has a different name
+        # than tmpdir!  Do a first run to get that name
         orig_dir = os.getcwd()
+        os.chdir(tmpdir)
+        tmpdir_actual_name = os.getcwd()
+        os.chdir(orig_dir)
 
         with DirStack() as ds:
             ds.push(tmpdir)
-            assert abspath(os.getcwd()) == abspath(tmpdir)
+            assert os.getcwd() == tmpdir_actual_name
 
-        assert abspath(os.getcwd()) == abspath(orig_dir)
+        assert os.getcwd() == orig_dir
 
 
 def test_dirstack_nocontext():
     import tempfile
     import os
-    from os.path import abspath
     from esutil.ostools import DirStack
 
     with tempfile.TemporaryDirectory() as tmpdir:
 
-        # try abspath to get tests to work on macos
+        # on macos CI the actual directory has a different name
+        # than tmpdir!  Do a first run to get that name
+
         orig_dir = os.getcwd()
+        os.chdir(tmpdir)
+        tmpdir_actual_name = os.getcwd()
+        os.chdir(orig_dir)
+
         ds = DirStack()
         ds.push(tmpdir)
-        assert abspath(os.getcwd()) == abspath(tmpdir)
+        assert os.getcwd() == tmpdir_actual_name
         ds.pop()
 
-        assert abspath(os.getcwd()) == abspath(orig_dir)
+        assert os.getcwd() == orig_dir
